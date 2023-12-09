73186e3d17002833b385eb7527619f9cb2a51d26
Sudolphus <micheal.hansen@gmail.com>
2023-12-08 20:54:54 -0800

    add tests for GetProductById

--- CatalogService.Tests/CatalogControllerTests.cs
+++ CatalogService.Tests/CatalogControllerTests.cs
@@ -1,13 +1,7 @@
 using CatalogService.Data;
 using CatalogService.Controllers;
 using CatalogService.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Moq;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 
@@ -18,20 +12,66 @@ public class CatalogControllerTests
     [Fact]
     public async Task GetProducts_ReturnsProducts()
     {
+        // Arrange
         using var connection = Helper.GetSqliteConnection();
         var options = Helper.GetSqliteContextOptions(connection);
         AddTestData(options);
 
         using var context = new CatalogContext(options);
         var controller = new CatalogController(context);
+
+        // Act
         var result = await controller.GetProducts();
-        var products = result.Value;
+
+        // Assert
+        Assert.NotNull(result);
+        var products = Assert.IsAssignableFrom<IEnumerable<Product>>(result.Value);
         Assert.Equal(2, products.Count());
         Assert.Collection(products,
             item => Assert.Equal(1, item.Id),
             item => Assert.Equal(2, item.Id)
         );
     }
+
+    [Fact]
+    public async Task GetProductById_ReturnsProduct_WhenExists()
+    {
+        // Arrange
+        using var connection = Helper.GetSqliteConnection();
+        var options = Helper.GetSqliteContextOptions(connection);
+        AddTestData(options);
+
+        using var context = new CatalogContext(options);
+        var controller = new CatalogController(context);
+
+        // Act
+        var product = (await controller.GetProductById(1)).Value;
+
+        // Assert
+        Assert.IsType<Product>(product);
+        Assert.Equal("Product 1", product.Name);
+    }
+
+    [Fact]
+    public async Task GetProductById_ReturnsNull_WhenProductDoesntExist()
+    {
+        // Arrange
+        using var connection = Helper.GetSqliteConnection();
+        var options = Helper.GetSqliteContextOptions(connection);
+        AddTestData(options);
+
+        using var context = new CatalogContext(options);
+        var controller = new CatalogController(context);
+
+        // Act
+        var product = (await controller.GetProductById(5)).Value;
+
+        // Assert
+        Assert.Null(product);
+    }
+
+
+    #region Helpers
 
     private static void AddTestData(DbContextOptions<CatalogContext> options)
     {
@@ -40,4 +80,6 @@ public class CatalogControllerTests
         context.Products.Add(new Product { Id = 2, Name = "Product 2", Price = 15.99M, Inventory = 10 });
         context.SaveChanges();
     }
+
+    #endregion
 }
