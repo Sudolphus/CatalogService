ed51e0bf6399992187dd34c2ecf5eaa99fa965fa
Sudolphus <micheal.hansen@gmail.com>
2023-12-08 21:33:37 -0800

    add test for CreateProduct

--- CatalogService.Tests/CatalogControllerTests.cs
+++ CatalogService.Tests/CatalogControllerTests.cs
@@ -3,12 +3,16 @@ using CatalogService.Controllers;
 using CatalogService.Models;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
+using Microsoft.AspNetCore.Http.Features;
 
 
 namespace CatalogService.Tests;
 
 public class CatalogControllerTests
 {
+    private static readonly Product product1 = new() { Id = 1, Name = "Product 1", Price = 9.99M, Inventory = 5 };
+    private static readonly Product product2 = new() { Id = 2, Name = "Product 2", Price = 15.99M, Inventory = 10 };
+
     [Fact]
     public async Task GetProducts_ReturnsProducts()
     {
@@ -70,15 +74,44 @@ public class CatalogControllerTests
         Assert.Null(product);
     }
 
+    [Fact]
+    public async Task CreateProduct_CreatesAndAdds()
+    {
+        // Arrange
+        using var connection = Helper.GetSqliteConnection();
+        var options = Helper.GetSqliteContextOptions(connection);
+
+        using var context = new CatalogContext(options);
+        var controller = new CatalogController(context);
+        string name = product1.Name;
+        decimal price = product1.Price;
+        int inventory = product1.Inventory;
+        var newProduct = CreateProduct(name, price, inventory);
+        
+        // Act
+        await controller.CreateProduct(newProduct);
+        var product = (await controller.GetProducts()).Value?.SingleOrDefault();
+
+        // Assert
+        Assert.IsType<Product>(product);
+        Assert.Equal(name, product.Name);
+        Assert.Equal(price, product.Price);
+        Assert.Equal(inventory, product.Inventory);
+    }
 
     #region Helpers
 
     private static void AddTestData(DbContextOptions<CatalogContext> options)
     {
         using var context = new CatalogContext(options);
-        context.Products.Add(new Product { Id = 1, Name = "Product 1", Price = 9.99M, Inventory = 5 });
-        context.Products.Add(new Product { Id = 2, Name = "Product 2", Price = 15.99M, Inventory = 10 });
+        context.Products.Add(product1);
+        context.Products.Add(product2);
         context.SaveChanges();
+    }
+
+    private static Product CreateProduct(string name, decimal price, int inventory)
+    {
+        return new Product { Name = name, Price = price, Inventory = inventory };
     }
 
     #endregion
